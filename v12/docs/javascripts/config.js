56412d34dc9a4c44da27da047016ed548494cd49
jerry-ubuntu <admin@zhamao.me>
2021-09-11 16:39:18 +0800

    add bdstatics

---
+++ v12/docs/javascripts/config.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+    (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?0dc75b6282f93d1976c16b51d899d301";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
